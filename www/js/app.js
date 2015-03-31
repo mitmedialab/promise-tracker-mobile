105bee831703972219f7c196c0f269ce7f98154c
emreiser <emilie.reiser@gmail.com>
2015-03-31 15:41:29 -0300

    Add globalization plugin to detect preferred language

--- www/js/app.js
+++ www/js/app.js
@@ -45,7 +45,7 @@ angular.module('ptApp', ['ionic', 'ptApp.controllers', 'ptApp.services', 'pascal
   }];
 })
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $translate) {
   
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -53,16 +53,21 @@ angular.module('ptApp', ['ionic', 'ptApp.controllers', 'ptApp.services', 'pascal
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
+
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
-  });
 
-  // Style iOS status bar
-  $ionicPlatform.ready(function() {
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+    if(typeof navigator.globalization !== "undefined") {
+      navigator.globalization.getPreferredLanguage(
+        function(language){
+          $translate.use((language.value).split("-")[0] == "en" ? "en" : "pt-BR");
+        }, 
+        function(error) {
+          console.log("ERROR -> " + error);
+        }
+      );
     }
   });
 })
@@ -240,5 +245,4 @@ angular.module('ptApp', ['ionic', 'ptApp.controllers', 'ptApp.services', 'pascal
   });
 
   $translateProvider.addInterpolation('$translateMessageFormatInterpolation');
-  $translateProvider.preferredLanguage('pt-BR');
 }]);
